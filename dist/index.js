ebccfe742b0f833f427b49f77a5910f17ea12bcb
Javed Hussein <jdhn42@gmail.com>
2023-08-01 15:48:39 -0400

    chore: add gitignore

--- dist/index.js
+++ dist/index.js
@@ -1,32 +1,21 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
+/**
+ * Copyright (C) 2022 - 2023 Permanent Data Solutions, Inc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PromiseCache = void 0;
-class PromiseCache {
-    constructor() {
-        this.store = {};
-    }
-    get(key, resolver) {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (!this.store[key]) {
-                this.store[key] = resolver();
-            }
-            return this.store[key];
-        });
-    }
-    clear(key) {
-        delete this.store[key];
-    }
-    clearAll() {
-        this.store = {};
-    }
-}
-exports.PromiseCache = PromiseCache;
+var promiseCache_1 = require("./promiseCache");
+Object.defineProperty(exports, "PromiseCache", { enumerable: true, get: function () { return promiseCache_1.PromiseCache; } });
